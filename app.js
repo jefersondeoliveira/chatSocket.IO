8f687861dcbbca75dd835ba7e79c68896af2779d
jefersondeoliveira <jefersondeoliveiralopes@gmail.com>
2015-08-07 12:45:48 -0300

    alteração configuração socket.io

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express')
   , load = require('express-load')
   , bodyParser = require('body-parser')
   , path  = require( "path" )
-  , app = express()
-  , socketio = require('socket.io');
-
+  , app = express.createServer(express.logger())
+  , io = require('socket.io').listen(app);
 
 app.set('port', (process.env.PORT || 3000));
+
+io.configure(function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -14,25 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use( "/public", express.static( path.join( __dirname, "public" ) ) );
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
-
 load('models')
   .then('controllers')
   .then('routes')
   .into(app);
-
-var server = http.createServer(app);
-io = socketio.listen(server, {log:false, origins:'*:*'});
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
 
 load('sockets')
         .into( io );
