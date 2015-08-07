4fa0c2cc48770c622fbef8ed88c8c7cd26d0c2d9
Jeferson Oliveira <jefersondeoliveiralopes@gmail.com>
2015-08-07 11:31:52 -0300

    Adicionando sockets

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express')
   , load = require('express-load')
   , bodyParser = require('body-parser')
   , path  = require( "path" )
-  , app = express();
+  , app = express()
+  , server = require('http').Server(app)
+  , io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -16,7 +18,10 @@ load('models')
   .then('controllers')
   .then('routes')
   .into(app);
+  
+load('sockets')
+        .into( io );
 
-app.listen(app.get('port'), function () {
+server.listen(app.get('port'), function () {
   console.log("Rodando na porta "+app.get('port'));
 });
